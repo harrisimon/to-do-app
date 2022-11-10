f4f6c5edd375252ba26abac36219e2cdf0471ffc
harrisimon <harrisimon@gmail.com>
2022-11-10 09:03:08 -0500

    initial files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+// import crossIcon from '../images/crossIcon.svg'
+import ToDo from './components/ToDo';
+
+function App() {
+  return (
+    <>
+      hi there
+      <ToDo />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ToDo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import moonIcon from '../images/icon-moon.svg'
+
+const ToDo = () => {
+
+    return(
+        <>
+            Some todos
+        </>
+    )
+}
+
+export default ToDo
